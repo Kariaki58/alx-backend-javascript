77804c65666f36e900d6cbb71f45d770399c86bd
kariaki58 <programmerjavascript901@gmail.com>
2024-03-04 17:34:18 +0100

    modified code

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,4 +1,4 @@
-export default function appendToEachArrayValue (array, appendString) {
+export default function appendToEachArrayValue(array, appendString) {
   for (const data of array) {
     array[array.indexOf(data)] = `${appendString}${data}`;
   }

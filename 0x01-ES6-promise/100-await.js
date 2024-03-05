8b7395508135330ccf76d3dc857564ad3c96ce91
kariaki58 <programmerjavascript901@gmail.com>
2024-03-05 13:09:21 +0100

    finsihed tasks

---
+++ 0x01-ES6-promise/100-await.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from "./utils";
+
+
+export default async function asyncUploadUser() {
+    const response1 = await uploadPhoto()
+    const response2 = await createUser()
+    return {
+        photo: response1,
+        user: response2
+    }
+}

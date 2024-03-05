73e816e9757355bb2e04a60e1ef052b9770ab312
kariaki58 <programmerjavascript901@gmail.com>
2024-03-05 18:32:13 +0100

    catch if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,9 @@
-export default function handleResponseFromAPI (promise) {
-  promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return new Error();
-    });
+export default function handleResponseFromAPI(promise) {
+  promise.then(data => {
+    console.log("Got a response from the API")
+    if (data === undefined) {
+      return new Error()
+    }
+   return {status: 200, body: 'success'}
+  })
 }

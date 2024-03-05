a39b8a29c2b9d8bdd11bc1eacea054414ce21519
kariaki58 <programmerjavascript901@gmail.com>
2024-03-05 18:36:12 +0100

    promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(data => {
-    console.log("Got a response from the API")
-    if (data === undefined) {
-      return new Error()
-    }
    return {status: 200, body: 'success'}
   })
+  .catch(err => new Error())
+  .finally(() => console.log("Got a response from the API"))
 }

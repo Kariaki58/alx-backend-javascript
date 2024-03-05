f18835634490ffd8bc5fc5668d57c6c533f0359c
kariaki58 <programmerjavascript901@gmail.com>
2024-03-05 19:03:35 +0100

    final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,15 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
-export default async function handleProfileSignup (firstName, lastName, filename) {
-  return Promise.all([signUpUser, uploadPhoto]);
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  .then(data => {
+    data.map(content => {
+      return {
+        status: content.status,
+        value: content.value
+      }
+    })
+  })
 }

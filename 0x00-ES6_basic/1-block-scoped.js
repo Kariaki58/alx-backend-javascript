335ecc34a72c54bb2d52dc968cd7fbe119e19421
kariaki58 <programmerjavascript901@gmail.com>
2024-03-06 09:39:46 +0100

    fixing es6

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,10 @@
 export default function taskBlock (trueOrFalse) {
-  if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-  }
   const task = false;
   const task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
   return [task, task2];
 }

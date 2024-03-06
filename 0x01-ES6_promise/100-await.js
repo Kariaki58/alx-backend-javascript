c853ead0e04a2177b6bc3c9047d45fa6cd1a4780
kariaki58 <programmerjavascript901@gmail.com>
2024-03-06 10:53:50 +0100

    promises

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,13 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function asyncUploadUser () {
+export default async function asyncUploadUser() {
   let json = {}
   try {
     const response1 = await uploadPhoto();
     const response2 = await createUser();
-    json = {photo: response1, user: response2}
+    json = {photo: response1, user: response2,}
   } catch (err) {
-    json = {photo: null, user: null}
+    json = {photo: null, user: null,}
   } finally {
     return json
   }

a17e653d88460804f472c2006051c04d0a5bd37e
kariaki58 <programmerjavascript901@gmail.com>
2024-03-06 10:51:18 +0100

    promises

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,11 @@
 export default function divideFunction(numerator, denominator) {
-  if (denominator === 0) {
-    throw new Error('Cannot divide by 0');
-  } else {
+  try {
+    if (denominator === 0) {
+      throw new Error('cannot divide by 0');
+    }
+
     return (numerator / denominator);
+  } catch(error) {
+    throw error;
   }
 }

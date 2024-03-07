d3446d909e6071b241f2692f1df0af8884b78984
kariaki58 <programmerjavascript901@gmail.com>
2024-03-07 16:56:25 +0100

    pricing class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,25 @@
+import Currency from "./3-currency";
+
+export default class Pricing {
+    constructor(amount, currency) {
+        this._amount = amount
+        this._currency = currency
+    }
+    get amount() {
+        return this._amount
+    }
+    set amount(pars) {
+        this._amount = pars
+    }
+    get currency() {
+        return this._currency
+    }
+    set currency(pars) {
+        this._currency.name = pars
+    }
+    displayFullPrice() {
+        const currency = this.currency.name
+        console.log()
+        return `${this.amount} ${currency} (${this.currency.code})`
+    }
+}

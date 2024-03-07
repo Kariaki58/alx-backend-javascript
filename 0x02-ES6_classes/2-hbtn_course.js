afe1cfba95e714336901f1505a999e8527657481
kariaki58 <programmerjavascript901@gmail.com>
2024-03-07 16:19:11 +0100

    two verification

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,7 +1,15 @@
 export default class HolbertonCourse {
     constructor(name, length, students) {
-        this._name = name
-        this._length = length
+        if (typeof name === "string") {
+            this._name = name
+        } else {
+            throw new TypeError("Name must be a string")
+        }
+        if (typeof length === "number") {
+            this._length = length
+        } else {
+            throw new TypeError("Length must be a number")
+        }
         this._students = students
     }
     get name() {

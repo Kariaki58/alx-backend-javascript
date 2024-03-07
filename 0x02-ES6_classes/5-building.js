b5fc2fefbae112bb628614d12b6efda20c630ee5
kariaki58 <programmerjavascript901@gmail.com>
2024-03-07 17:36:28 +0100

    creating error

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,11 +1,17 @@
 export default class Building {
     constructor(sqft) {
-        this._sqft = sqft
+        if (this.constructor !== Building) {
+            if (typeof this.evacuationWarningMessage !== 'function') {
+              throw new Error(
+                'Class extending Building must override evacuationWarningMessage',
+              );
+            }
+        }
+        this._sqft = sqft;
     }
+
     get sqft() {
-        return this._sqft
-    }
-    evacuationWarningMessage() {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
+        return this._sqft;
     }
 }
+

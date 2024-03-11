43bce25baab16c3179d2e0e376182815703d0766
kariaki58 <programmerjavascript901@gmail.com>
2024-03-11 16:33:17 +0100

    more map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(map){
+    if (map.constructor === Map) {
+        map.forEach((_, data) => {
+            if (map.get(data) === 1) {
+                map.set(data, 100)
+            }
+        })
+        return map
+    }
+    throw new Error('Cannot process')
+}

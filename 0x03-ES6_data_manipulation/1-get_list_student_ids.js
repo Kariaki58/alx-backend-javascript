b02066a429f863575fb113a4a7235abdb38bbd7d
kariaki58 <programmerjavascript901@gmail.com>
2024-03-11 11:03:26 +0100

    More mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(objList){
+    if (typeof objList !== "object") {
+        return []
+    }
+    return objList.map(item => {
+        return item.id
+    })
+}

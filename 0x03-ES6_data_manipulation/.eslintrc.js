acf909bf30d6308c37e5f6233aaaf17e2de8b06a
kariaki58 <programmerjavascript901@gmail.com>
2024-03-12 13:42:25 +0100

    configuration

--- 0x03-ES6_data_manipulation/.eslintrc.js
+++ 0x03-ES6_data_manipulation/.eslintrc.js
@@ -1,17 +1,25 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true
-  },
-  extends: 'eslint:recommended',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
-  rules: {
-  }
+    "root": true,
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
 };

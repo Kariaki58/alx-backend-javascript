00db553fdb4194eaa996f50d25b345d0ce335c11
kariaki58 <programmerjavascript901@gmail.com>
2024-03-12 14:21:46 +0100

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,10 @@
 export default function cleanSet (set, startString) {
   const resultArray = [];
   set.forEach(item => {
-    if (item.startsWith(startString)) {
+    if (item.startsWith(startString) && startString !== '') {
       resultArray.push(item.substring(startString.length));
+    } else {
+      return ''
     }
   });
   return resultArray.join('-');

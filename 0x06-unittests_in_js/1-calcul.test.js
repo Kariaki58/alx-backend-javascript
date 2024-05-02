c53b49923445cb90a8c3a77ed62b2b35caf949df
kariaki58 <kariakistephen809@gmail.com>
2024-05-02 15:27:21 +0100

    test check task 1

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,54 @@
+const calculateNumber = require('./1-calcul.js')
+const assert = require('assert')
+
+
+describe("calculateNumber", () => {
+    it("check correct output", () => {
+        assert.strictEqual(calculateNumber(1, 2, 'SUM'), 3)
+    })
+    it('check correct output', () => {
+        assert.strictEqual(calculateNumber(-1, 2, 'SUM'), 1)
+    })
+    it ('check correct ouput', () => {
+        assert.strictEqual(calculateNumber(-1, -2, 'SUM'), -3)
+    })
+    it ('check correct output', () => {
+        assert.strictEqual(calculateNumber(0, 2, 'SUM'), 2)
+    })
+    it ('check correct output', () => {
+        assert.strictEqual(calculateNumber(2.1, 3.5, 'SUM'), 6)
+    })
+    it("check correct output", () => {
+        assert.strictEqual(calculateNumber(3.5, 4.5, 'SUM'), 9)
+    })
+    it ("check correct output", () => {
+        assert.strictEqual(calculateNumber(0, 0, 'SUM'), 0)
+    })
+    it ('check correct ouput', () => {
+        assert.strictEqual(calculateNumber(3, 2, 'SUBTRACT'), 1)
+    })
+    it ('check correct ouput', () => {
+        assert.strictEqual(calculateNumber(3.5, 2.2, 'SUBTRACT'), 2)
+    })
+    it ('check correct ouput', () => {
+        assert.strictEqual(calculateNumber(5.3, 2.7, 'SUBTRACT'), 2)
+    })
+    it ('check correct ouput', () => {
+        assert.strictEqual(calculateNumber(1, 2, 'SUBTRACT'), -1)
+    })
+    it('check correct ouput', () => {
+        assert.strictEqual(calculateNumber(1.1, 0, 'SUBTRACT'), 1)
+    })
+    it ('check correct ouput', () => {
+        assert.strictEqual(calculateNumber(2, 2, 'DIVIDE'), 1)
+    })
+    it ('check correct ouput', () => {
+        assert.strictEqual(calculateNumber(4.2, 2.3, 'DIVIDE'), 2)
+    })
+    it ('check correct ouput', () => {
+        assert.strictEqual(calculateNumber(5.5, 2.5, 'DIVIDE'), 2)
+    })
+    it ('check correct ouput', () => {
+        assert.strictEqual(calculateNumber(3, 0, 'DIVIDE'), "Error")
+    })
+})

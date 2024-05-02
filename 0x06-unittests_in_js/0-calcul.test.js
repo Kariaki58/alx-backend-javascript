db6003f1ccd741095fa8cb385ad82600ed0953f6
kariaki58 <kariakistephen809@gmail.com>
2024-05-02 13:35:07 +0100

    first test code

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert')
+const calculateNumber = require('./0-calcul')
+
+
+describe('calculateNumber', () => {
+    it('0 and a whole number', () => {
+        assert.strictEqual(calculateNumber(0, 1), 1)
+    })
+    it("rounded both floating point number", () => {
+        assert.strictEqual(calculateNumber(1.5, 3.2), 5)
+    })
+    it('rounded one of the input', () => {
+        assert.strictEqual(calculateNumber(1.5, 3), 5)
+    })
+    it('rounded b input of the function', () => {
+        assert.strictEqual(calculateNumber(3, 1.5), 5)
+    })
+})

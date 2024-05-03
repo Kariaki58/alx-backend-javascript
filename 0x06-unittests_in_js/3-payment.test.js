4569d85cfb2c9037882357745949f0969b423390
kariaki58 <kariakistephen809@gmail.com>
2024-05-03 02:32:53 +0100

    task 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,3 +1,8 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
 describe('sendPaymentRequestToApi', () => {
 	it('check correct output', () => {
 		const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
